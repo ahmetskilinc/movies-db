7f91a79b2b4cc1004a9f50e2f580261774f81892
Ahmet Kilinc <akx9@icloud.com>
2022-04-03 20:15:35 +0100

    added some more

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({ movie, type }) => {
 		<Link to={`${type === "tv" ? "tv" : "movie"}/${id}`} className="h-48 w-32 md:h-80 md:w-52 flex-shrink-0 shadow-md rounded-xl relative">
 			<div
 				className="h-full w-full rounded-xl bg-cover bg-center"
-				style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${poster_path})` }}
+				style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w185${poster_path})` }}
 			></div>
 		</Link>
 	);
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -44,23 +44,24 @@ const Movie = () => {
 	return movie !== null ? (
 		<div
 			className="w-full py-8 relative after:content-[''] after:bg-slate-500 after:w-full after:h-full after:bg-opacity-60 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-[1] bg-cover bg-center"
-			style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})` }}
+			style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}
 		>
 			<div className="flex flex-col lg:flex-row justify-center items-center mx-auto lg:w-cs relative z-10 px-6 lg:p-0 ">
 				<img
 					loading="lazy"
-					src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`}
+					src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
 					alt={movie.title}
 					className="w-44 lg:w-72 rounded-xl shadow-2xl lg:mr-6 "
 				/>
 				<div>
 					<h1 className="text-xl lg:text-5xl font-bold text-white">{movie.title}</h1>
+					<p className="text-gray-300 pt-2">
+						{movie.genres.map((genre, index) => `${genre.name}${index === movie.genres.length - 1 ? "" : ", "}`)}
+						{" - "}
+						{format(parseISO(movie.release_date), "MMMM dd, yyyy")}
+					</p>
 					<p className="py-3 text-white">{movie.overview}</p>
-					<div>
-						<p className="text-white">
-							<span className="font-bold">Release Date:</span> {format(parseISO(movie.release_date), "MMMM dd, yyyy")}
-						</p>
-					</div>
+					<div></div>
 				</div>
 			</div>
 		</div>
