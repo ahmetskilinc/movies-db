7c55fa3d62693a08b737f455aed2ba457f855e18
Ahmet Kilinc <akx9@icloud.com>
2022-04-09 22:55:40 +0100

    smaller images for mobile

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -3,12 +3,12 @@ function Cast({ credits }) {
 		<div className="w-full py-8">
 			<div className="flex items-start mx-auto gap-4 lg:w-cs z-10 px-6 lg:p-0 overflow-scroll">
 				{credits.cast.map((character, index) => (
-					<div className="w-44 flex-shrink-0" key={index}>
+					<div className="w-16 lg:w-44 flex-shrink-0" key={index}>
 						<img
 							loading="lazy"
-							src={`https://image.tmdb.org/t/p/w342${character.profile_path}`}
+							src={`https://image.tmdb.org/t/p/w185${character.profile_path}`}
 							alt={character.name}
-							className="w-44 ratio-4x3 object-cover rounded-xl shadow-2xl lg:mr-6"
+							className="w-16 lg:w-44 ratio-4x3 object-cover rounded-xl shadow-2xl lg:mr-6"
 						/>
 						<div>
 							<h1 className="text-md lg:text-xl font-bold text-white">{character.name}</h1>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Nav } from "./Nav";
 export { default as MovieCard } from "./MovieCard";
 export { default as MoviesList } from "./MoviesList";
 export { default as HomeHero } from "./HomeHero";
+export { default as Hero } from "./Hero";
+export { default as Cast } from "./Cast";
