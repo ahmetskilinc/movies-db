f46ea9fddbd02e3332aacf65d8ea01ca70195f39
Ahmet Kilinc <akx9@icloud.com>
2022-04-13 13:36:05 +0100

    change movie card to image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,19 @@
+import { isMobile } from "react-device-detect";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie, type }) => {
 	const { poster_path, id } = movie;
 
 	return (
-		<Link to={`${type === "tv" ? "tv" : "movie"}/${id}`} className="h-48 w-32 md:h-80 md:w-52 flex-shrink-0 shadow-md rounded-xl relative">
-			<div
-				className="h-full w-full rounded-xl bg-cover bg-center"
-				style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w185${poster_path})` }}
-			></div>
+		<Link to={`${type === "tv" ? "tv" : "movie"}/${id}`} className="flex-shrink-0 shadow-md rounded-xl relative">
+			<img
+				loading="lazy"
+				src={poster_path ? `https://image.tmdb.org/t/p/w185${poster_path}` : "/images/placeholder.jpeg"}
+				alt={movie.title}
+				width={isMobile ? "96px" : "192px"}
+				height={isMobile ? "144px" : "288px"}
+				className="w-24 lg:w-48 h-36 lg:h-72 object-cover rounded-xl shadow-2xl"
+			/>
 		</Link>
 	);
 };
