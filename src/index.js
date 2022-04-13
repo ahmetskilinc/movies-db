ec03f6dc31a0ec30d859fc5169e3c86453039d46
Ahmet Kilinc <akx9@icloud.com>
2022-04-13 14:17:24 +0100

    service worker & vercel.json

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -13,3 +15,5 @@ root.render(
 		</BrowserRouter>
 	</React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
