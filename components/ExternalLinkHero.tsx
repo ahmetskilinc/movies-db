e29aecac363b0a5891e2f5f3e29425a64006b5b4
Ahmet Kilinc <akx9@icloud.com>
2022-04-16 23:22:55 +0100

    fix

--- components/ExternalLinkHero.tsx
+++ components/ExternalLinkHero.tsx
@@ -1,7 +1,7 @@
 const ExternalLinkHero = (props: { link: string; title: string }) => {
 	const { link, title } = props;
 	return (
-		<a href={link} className="hover:underline flex items-center" target="_blank">
+		<a href={link} className="hover:underline flex items-center" target="_blank" rel="noreferrer">
 			{title}
 			<svg xmlns="http://www.w3.org/2000/svg" className="h-[18px] w-[18px]" viewBox="0 0 20 20" fill="currentColor">
 				<path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
