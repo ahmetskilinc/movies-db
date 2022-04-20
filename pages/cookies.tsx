30c744d5c2af96db2bc23db7f36457094574a292
Ahmet Kilinc <akx9@icloud.com>
2022-04-20 20:04:51 +0100

    dangerous

--- pages/cookies.tsx
+++ pages/cookies.tsx
@@ -1,7 +1,7 @@
 const Cookies = () => {
 	return (
 		<article className="w-full my-2 md:my-4 mx-auto lg:max-w-cs px-cs">
-			<p>I don't use cookies.</p>
+			<p>I don&apos;t use cookies.</p>
 		</article>
 	);
 };
