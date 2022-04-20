215ab3e9e1f98f4443927de16b537e48ad505a78
Ahmet Kilinc <akx9@icloud.com>
2022-04-20 10:46:54 +0100

    fix fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	}, [router.events]);
 	return (
 		<>
+			<Script src="../js/index.min.js" strategy="lazyOnload" />
 			{process.env.NEXT_PUBLIC_ENV === "production" ? (
 				<>
 					<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,6 @@ export default function Document() {
 			<Head>
 				<meta charSet="utf-8" />
 				<meta name="keywords" content="Movies, TV Shows, Popular Movies, Movie, Tv Show, Series, MovieDB, Ahmet, Kilinc, Ahmet Kilinc, AhmetK" />
-
-				<script src="./js/index.min.js"></script>
 			</Head>
 			<body className="pt-16">
 				<Main />
