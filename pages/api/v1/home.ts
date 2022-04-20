7958ddfbd5bb595b34a4371b0a51c067bc109d75
Ahmet Kilinc <akx9@icloud.com>
2022-04-20 14:57:58 +0100

    shorten

--- pages/api/v1/home.ts
+++ pages/api/v1/home.ts
@@ -1,11 +1,15 @@
+import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 // import axios from "axios";
 
+const endpoint = process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT;
+const key = process.env.NEXT_PUBLIC_TMDB_KEY;
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const responseHomeHeroMovie = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const responseHomeHeroMovie = await fetch(`${endpoint}trending/movie/day?api_key=${key}`);
 	const dataHomeHeroMovie = await responseHomeHeroMovie.json();
 
-	const responseHomeHeroTv = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}trending/tv/day?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const responseHomeHeroTv = await fetch(`${endpoint}trending/tv/day?api_key=${key}`);
 	const dataHomeHeroTv = await responseHomeHeroTv.json();
 
 	const trendingToday = [
@@ -15,12 +19,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		{ obj: dataHomeHeroTv.results[1], type: "tv", title: "Trending TV Show" },
 	];
 
-	const responseMovies = await fetch(
-		`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&language=en-US&page=1`
-	);
+	const responseMovies = await fetch(`${endpoint}movie/popular?api_key=${key}&language=en-US&page=1`);
 	const dataMovies = await responseMovies.json();
 
-	const responseTv = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}tv/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&language=en-US&page=1`);
+	const responseTv = await fetch(`${endpoint}tv/popular?api_key=${key}&language=en-US&page=1`);
 	const dataTv = await responseTv.json();
 
 	return res.status(200).json({
--- pages/api/v1/movie.ts
+++ pages/api/v1/movie.ts
@@ -1,28 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // import axios from "axios";
 
+const endpoint = process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT;
+const key = process.env.NEXT_PUBLIC_TMDB_KEY;
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const { id } = req.query;
-	const movie = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const movie = await fetch(`${endpoint}movie/${id}?api_key=${key}`);
 	const movieData = await movie.json();
 
-	const movieCredits = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}/credits?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const movieCredits = await fetch(`${endpoint}movie/${id}/credits?api_key=${key}`);
 	const movieCreditsData = await movieCredits.json();
 
-	const movieRecommendations = await fetch(
-		`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}/recommendations?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
-	);
+	const movieRecommendations = await fetch(`${endpoint}movie/${id}/recommendations?api_key=${key}`);
 	const movieRecommendationsData = await movieRecommendations.json();
 
-	const movieExternalIds = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}/external_ids?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const movieExternalIds = await fetch(`${endpoint}movie/${id}/external_ids?api_key=${key}`);
 	const movieExternalIdsData = await movieExternalIds.json();
 
-	const movieReviews = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}/reviews?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`);
+	const movieReviews = await fetch(`${endpoint}movie/${id}/reviews?api_key=${key}`);
 	const movieReviewsData = await movieReviews.json();
 
-	const movieWatchProviders = await fetch(
-		`${process.env.NEXT_PUBLIC_DEFAULT_ENDPOINT}movie/${id}/watch/providers?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
-	);
+	const movieWatchProviders = await fetch(`${endpoint}movie/${id}/watch/providers?api_key=${key}`);
 	const movieWatchProvidersData = await movieWatchProviders.json();
 
 	return res.status(200).json({
